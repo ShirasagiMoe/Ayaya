986be6f1ce0c5f438b235a2daa88d255f09731db
Sendya <18x@loacg.com>
2019-01-02 01:02:27 +0800

    fix: GLOBAL ver undef

--- .webpack/prod.js
+++ .webpack/prod.js
@@ -1,12 +1,18 @@
 // env: node
 var webpack = require("webpack");
 const extractTextPlugin = require("extract-text-webpack-plugin");
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const GitRevision = new GitRevisionPlugin();
 
 module.exports = function (options) {
     delete options.entry.bundle;
     options.devtool = 'cheap-module-source-map';
     options.plugins = [
         new extractTextPlugin("[name].css"),
+        new webpack.DefinePlugin({
+            AYAYA_VERSION: `"${require('../package.json').version}"`,
+            GIT_HASH: JSON.stringify(GitRevision.version())
+        })
     ];
     options.plugins.unshift(new webpack.LoaderOptionsPlugin({
         minimize: true,
